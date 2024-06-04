da9a8f052222d77400e36a4b959fbfdf83cc0b01
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 15:45:38 -0500

    Pattern 1 Done

---
+++ Patterns/Patterns.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Patterns{
+    public static void printPattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern1(n);
+    }
+}
