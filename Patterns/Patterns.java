5fa7574126b65a9b18cc3f369f3a84409957b2e5
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 15:52:11 -0500

    Pattern 4 added in the Patterns.java file

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -28,6 +28,15 @@ class Patterns{
         }
     }
 
+    public static void printPattern4(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println("---------- Pattern 1 ----------");
@@ -36,5 +45,7 @@ class Patterns{
         printPattern2(n);
         System.out.println("---------- Pattern 3 ----------");
         printPattern3(n);
+        System.out.println("---------- Pattern 4 ----------");
+        printPattern4(n);
     }
 }
