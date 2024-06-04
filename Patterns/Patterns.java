d6aee7e074cc87e791d66d205fb9b0a3acb51edd
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 15:48:30 -0500

    Pattern 2 added in the Patterns.java file

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -9,8 +9,21 @@ class Patterns{
             System.out.println();
         }
     }
+
+    public static void printPattern2(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
+        System.out.println("---------- Pattern 1 ----------");
         printPattern1(n);
+        System.out.println("---------- Pattern 2 ----------");
+        printPattern2(n);
     }
 }
