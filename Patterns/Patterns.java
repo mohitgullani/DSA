f6d9e98ccdedaace5d3c87696618d1d7df56654e
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 15:50:38 -0500

    Pattern 3 added in the Patterns.java file

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -19,11 +19,22 @@ class Patterns{
         }
     }
 
+    public static void printPattern3(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println("---------- Pattern 1 ----------");
         printPattern1(n);
         System.out.println("---------- Pattern 2 ----------");
         printPattern2(n);
+        System.out.println("---------- Pattern 3 ----------");
+        printPattern3(n);
     }
 }
