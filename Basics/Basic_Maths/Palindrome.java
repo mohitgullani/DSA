f2f5c74f2d9d160dec6cafdc4ad58f8b54450aca
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 21:49:59 -0500

    Palindrome Number TC -> O(log10(n)) and SC -> O(1)

---
+++ Basics/Basic_Maths/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        int reverse = 0;
+        int temp = x;
+        while(temp > 0){
+            int digit = temp % 10;
+            reverse = (reverse * 10) + digit;
+            temp /= 10;
+        }
+
+        return (x == reverse) ? true : false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(452));
+        System.out.println(isPalindrome(74547));
+    }
+}
