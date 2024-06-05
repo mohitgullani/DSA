008dc412194b3b81646e9ed2fcce5afbefc01403
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 22:22:42 -0500

    Print All Divisors using 2 ways Best way {TC -> O(N) and SC -> O(1)} and Optimal way {TC -> O(Sqrt(N)) and SC -> O(1)}

--- Basics/Basic_Maths/All_Divisors.java
+++ Basics/Basic_Maths/All_Divisors.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class All_Divisors {
-    // Time Complexity -> O(N) and SC -> O(1)
+    // Time Complexity -> O(N)
     // Space Complexity is O(1) because I am using List to store the divisors, I'm not using to solve the problem
-    public static List<Integer> divisors(int n){
+    public static List<Integer> divisorsApproach1(int n){
         List<Integer> list = new ArrayList<>();
         for(int i=1;i<=n;i++){
             if(n % i == 0){
@@ -12,8 +12,38 @@ public class All_Divisors {
         }
         return list;
     }
+
+    // Time Complexity -> O(Sqrt(N))
+    // Space Complexity is O(1) because I am using List to store the divisors, I'm not using to solve the problem
+    public static List<Integer> divisorsApproach2(int n){
+        /* For N = 36
+            1 * 36
+            2 * 18
+            3 * 12
+            4 * 9
+            6 * 6
+            9 * 4
+            12 * 3
+            18 * 2
+            36 * 1
+         */
+        List<Integer> list = new ArrayList<>();
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n % i == 0){
+                list.add(i);
+
+                if((n/i) != i){ 
+                    list.add(n/i);
+                }
+            }
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        List<Integer> list = divisors(36);
-        System.out.println(list);
+        List<Integer> list1 = divisorsApproach1(36);
+        System.out.println(list1);
+        List<Integer> list2 = divisorsApproach2(36);
+        System.out.println(list2);
     }
 }
