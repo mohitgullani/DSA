f2c42d7684ee9661ed8b910ca0206eb1528a4482
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 21:25:56 -0500

    Count Digits using 2 ways TC -> log10(n) & SC -> O(1)

--- Basics/Basic_Maths/Count_Digit.java
+++ Basics/Basic_Maths/Count_Digit.java
@@ -11,13 +11,13 @@ class Count_Digit{
         return count;
     }
 
-    // Used inbuilt log10 function to find digits
+    // Used inbuilt log10 function to find digits.
     public static int countDigit2(int n){
         return (int)Math.log10(n) + 1;
     }
     public static void main(String[] args) {
         int n = 52896;
-        System.out.println("Digits in the given number " + n + " is " + countDigit1(n));
-        System.out.println("Digits in the given number " + n + " is " + countDigit2(n));
+        System.out.println("Digits in the given number using function 1 " + n + " is " + countDigit1(n));
+        System.out.println("Digits in the given number using function 2 " + n + " is " + countDigit2(n));
     }
 }
