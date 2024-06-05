119a0760be3a4810a528055adc7bad46ae78a947
Mohit Gullani <mohitgullani9@gmail.com>
2024-06-04 22:11:46 -0500

    Print All Divisors TC -> O(N) and SC -> O(1)

--- Basics/Basic_Maths/All_Divisors.java
+++ Basics/Basic_Maths/All_Divisors.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class All_Divisors {
     // Time Complexity -> O(N) and SC -> O(1)
-    // Space Complexity is O(1) because I am using list to store the divisors, I'm not using to solve the problem
+    // Space Complexity is O(1) because I am using List to store the divisors, I'm not using to solve the problem
     public static List<Integer> divisors(int n){
         List<Integer> list = new ArrayList<>();
         for(int i=1;i<=n;i++){
